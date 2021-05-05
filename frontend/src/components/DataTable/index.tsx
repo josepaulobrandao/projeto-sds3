a994e267e1dc00cd2787966c35dd14ea381921a9
Jose Paulo <paulobrandao356@gmail.com>
2021-05-04 23:32:05 -0300

    changing datatable

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -13,7 +13,14 @@ function DataTable() {
         </thead>
         <tbody>
             <tr>
-                <td>22/04/2021</td>
+                <td>17/03/2021</td>
+                <td>Amelia Smith</td>
+                <td>24</td>
+                <td>12</td>
+                <td>15477.00</td>
+            </tr>
+            <tr>
+                <td>18/10/1995</td>
                 <td>Barry Allen</td>
                 <td>34</td>
                 <td>25</td>
@@ -27,78 +34,71 @@ function DataTable() {
                 <td>15017.00</td>
             </tr>
             <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>17/04/2018</td>
+                <td>Murphy Johnson</td>
+                <td>48</td>
+                <td>17</td>
+                <td>2048.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
+                <td>Williams O'Sullivan</td>
+                <td>32</td>
                 <td>25</td>
-                <td>15017.00</td>
+                <td>1554.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
+                <td>Brown Walsh</td>
                 <td>34</td>
                 <td>25</td>
-                <td>15017.00</td>
+                <td>15048.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Taylor Smith</td>
+                <td>28</td>
+                <td>11</td>
+                <td>1546.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Davies 	O'Brien</td>
+                <td>30</td>
+                <td>27</td>
+                <td>1566.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Wilson Davis</td>
+                <td>28</td>
+                <td>18</td>
+                <td>1577.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
+                <td>Brown Jones</td>
+                <td>33</td>
                 <td>25</td>
-                <td>15017.00</td>
+                <td>1457.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Thomas Rodriguez</td>
+                <td>33</td>
+                <td>19</td>
+                <td>1954.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
+                <td>O'Neill	 Wilson</td>
+                <td>45</td>
                 <td>25</td>
-                <td>15017.00</td>
+                <td>6466.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
+                <td>Roberts Johnson</td>
                 <td>34</td>
                 <td>25</td>
                 <td>15017.00</td>
