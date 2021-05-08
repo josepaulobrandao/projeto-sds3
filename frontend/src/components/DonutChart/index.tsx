7cb09a4f2433ed453a3e420149f7bc1b1c39f2ac
Jose Paulo <paulobrandao356@gmail.com>
2021-05-08 13:35:28 -0300

    Type 'String[]' is not assignable to type 'string[]'

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,28 +1,30 @@
 import axios from 'axios';
-import { send } from 'node:process';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
-    labels:string[];
-    series:number[];
+    labels: string [];
+    series: number[];
 }
 
 
 function DonutChart() {
+
+    const [chartData,setChartData] = useState<ChartData>({labels: [], series: []});
+
     //forma errada
-    let chartData: ChartData = {labels: [], series: []};
     //forma errada
-   axios.get(`${BASE_URL} + /sales/sales/amount-by-seller`)
+   axios.get(`${BASE_URL}/sales/sales/amount-by-seller`)
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series:mySeries};
+            setChartData({labels: myLabels, series:mySeries});
             console.log(response.data);
-        })
+        }); 
    /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
